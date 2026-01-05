0bca9bdb5670e7346bf4bd155cd8c3e4eeff9614
AaryanBaral <aryanbaral1100@gmail.com>
2026-01-05 22:17:48 +0545

    CA certifcated added

--- src/db.js
+++ src/db.js
@@ -5,10 +5,14 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const ca = process.env.PG_CA_CERT?.replace(/\\n/g, "\n");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized: false,
+    ca
+  },
 });
 
 export const query = (text, params) => pool.query(text, params);
-
 export default pool;
